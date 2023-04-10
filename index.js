bdf181d6e98c640c2af6241a917122a9fb6ad263
varshithno1 <76397657+varshithno1@users.noreply.github.com>
2023-04-11 02:11:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const nav = document.querySelector(".wrap");
 const hero = document.querySelector(".hero-wrap");
 const more = document.querySelector(".matter");
+const herop = document.querySelector(".hide");
+const herobtn = document.querySelector(".hero-btn");
+const heroa = document.querySelector(".know-more");
+
+herobtn.addEventListener("click", () => {
+    if(herop.style.display == "none"){
+        herop.style.display = "inline"
+        heroa.innerText = "Less"
+    }
+    else{
+        herop.style.display = "none"
+        heroa.innerText = "Know More"
+    }
+});
 
 const heroOptions = {
     threshold: 0.25
